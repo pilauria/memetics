603c93730499e6bbcc777179d78a604c2624788d
pilauria <ptr.lauria@gmail.com>
2021-11-19 09:42:20 +0100

    add marco template

--- app.js
+++ app.js
@@ -1,54 +1,28 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-
-var indexRouter = require('./routes/index.route');
-var usersRouter = require('./routes/users.route');
-var roomsRouter = require('./routes/room.route');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
-require('./db');
+// Functional curling style of loading configuration
+require('./config/db')
+require('./config/global')(app)
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 24 * 60 * 60 * 1000 },
-    store: MongoStore.create({
-      mongoUrl: process.env.MONGODB_URI,
-      ttl: 24 * 60 * 60,
-    }),
-  })
-);
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/rooms', roomsRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- models/User.model.js
+++ models/User.model.js
@@ -1,9 +1,23 @@
 const { Schema, model } = require('mongoose');
 
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
-  username: String,
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	// favorites: [{ type: Schema.Types.ObjectId, ref: 'Room', default: [] }]
 });
 
-module.exports = model('User', userSchema);
+const User = model('User', userSchema);
+
+module.exports = User;
