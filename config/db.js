003f188b17292566c04bb6ff7377a3fbe79682ac
pilauria <ptr.lauria@gmail.com>
2021-11-19 17:48:47 +0100

    create login-signup-forms

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URL = `${process.env.MONGODB_URI}/${process.env.DB_NAME}`;
+const MONGO_URL = process.env.MONGODB_URI;
 
-console.log("MONGO_URL: ", MONGO_URL)
+console.log('MONGO_URL: ', MONGO_URL);
 mongoose
   .connect(MONGO_URL, {
     useNewUrlParser: true,
@@ -15,9 +15,11 @@ mongoose
     // useFindAndModify: false,
     // useCreateIndex: true
   })
-  .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  .then(x => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
+  .catch(err => {
+    console.error('Error connecting to mongo: ', err);
   });
