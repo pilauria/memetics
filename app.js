9d8946e580cc16b28d73ea7c5e1d2f1925ca3383
pilauria <ptr.lauria@gmail.com>
2021-11-21 12:16:05 +0100

    create routes list+create

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-require('dotenv').config()
+require('dotenv').config();
 
 var createError = require('http-errors');
 var express = require('express');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const memeRoutes = require('./routes/meme');
 
 var app = express();
 
 // Functional curling style of loading configuration
-require('./config/db')
-require('./config/global')(app)
-
+require('./config/db');
+require('./config/global')(app);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/memes', memeRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
