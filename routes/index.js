520441dfb58d0053c998ccd3d3bb8991a42e9ac4
pilauria <ptr.lauria@gmail.com>
2021-11-24 13:46:48 +0100

    remove line in index.js

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 const User = require('../models/User.model');
-const Api = require('../apis/api');
 
 /* GET home page. */
 router.get('/', (req, res) => {
   const isAuthorized = req.session.currentUser ? true : false;
-  if(isAuthorized === true){
+  if (isAuthorized === true) {
     let userName = req.session.currentUser.username.charAt(0).toUpperCase();
     User.find().then(users =>
       res.render('index', { users, isAuthorized, userName })
     );
-  }else{
-    User.find().then(users =>
-      res.render('index', { users, isAuthorized })
-    );
+  } else {
+    User.find().then(users => res.render('index', { users, isAuthorized }));
   }
 });
-
 
 module.exports = router;
