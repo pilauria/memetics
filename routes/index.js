5f285dade9cd79991d3930d6beebe847050197d2
pilauria <ptr.lauria@gmail.com>
2021-11-25 00:07:12 +0100

    css some

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,29 @@
 var express = require('express');
 var router = express.Router();
-const MemeApi = require('../apis/api');
 
+const MemeApi = require('../apis/api');
 const User = require('../models/User.model');
-const Api = require('../apis/api');
 
 /* GET home page. */
 router.get('/', async (req, res) => {
-  const getMemes = await MemeApi.getAll();
-  const allMemes = getMemes.data.data.memes;
-  const fewMemes = allMemes.filter(meme => meme.height < 380);
-  const isAuthorized = req.session.currentUser ? true : false;
-  if (isAuthorized === true) {
-    let userName = req.session.currentUser.username.charAt(0).toUpperCase();
-    User.find().then(users => {
-      res.render('index', { users, isAuthorized, userName, fewMemes });
-    });
-  } else {
-    User.find().then(users =>
-      res.render('index', { users, isAuthorized, fewMemes })
-    );
+  try {
+    const getMemes = await MemeApi.getAll();
+    const allMemes = getMemes.data.data.memes;
+    const fewMemes = allMemes.filter(meme => meme.height < 380);
+    const isAuthorized = req.session.currentUser ? true : false;
+    if (isAuthorized === true) {
+      let userName = req.session.currentUser.username.charAt(0).toUpperCase();
+      User.find().then(users => {
+        res.render('index', { users, isAuthorized, userName, fewMemes });
+      });
+    } else {
+      User.find().then(users =>
+        res.render('index', { users, isAuthorized, fewMemes })
+      );
+    }
+  } catch (error) {
+    console.log(error);
   }
-});
-
-/* GET from API */
-router.get('/api', (req, res) => {
-  Api.getAll().then(entity =>
-    res.render('index', { title: 'Express', users: entity })
-  );
 });
 
 module.exports = router;
